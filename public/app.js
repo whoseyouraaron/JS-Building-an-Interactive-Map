6af5011d9741f063ac9b84c1c34ffd92f36c088f
whoseyouraaron <whoseyouraaron@yahoo.com>
2022-11-23 19:25:20 -0800

    changed code

--- public/app.js
+++ public/app.js
@@ -1,7 +1,15 @@
-// create map
-
+// create
+const mMap = L.map('map', {
+    zoom: 12,
+    center: [48.868672, 2.342130]
+})
 
 // add openstreetmap tiles
+// Add OpenStreetMap tiles:
+L.tileLayer('https://.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
 // create and main add geolocation marker
@@ -14,6 +22,5 @@
 
 
 // metro station markers
-
 
 
